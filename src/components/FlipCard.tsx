2c5385fb6a472ae055f3df30843a3017d0e34874
lifeissad <145711984+Anarchist1984@users.noreply.github.com>
2024-12-24 15:00:00 +0000

    Removed unecessary import

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { Github, Instagram, MessageSquare, Mail } from 'lucide-react';
 import GlitchImage from './GlitchImage';
 import GlitchIcon from './GlitchIcon';
